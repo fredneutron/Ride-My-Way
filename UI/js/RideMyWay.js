e1080ca18a9c71b1cd0ffda648f13a911f4fd747
Fred Adelabu <adelabu4fred@gmail.com>
2018-06-12 19:36:00 +0100

    fixing Search box

--- UI/js/RideMyWay.js
+++ UI/js/RideMyWay.js
@@ -1,5 +1,7 @@
 let signin = document.getElementById('on');
 let signup = document.getElementById('off'); 
+let caution = document.getElementById('switch');
+let x;
 document.getElementsByClassName('close')[0].onclick =() =>{
     signin.style.display = "none";
 }
@@ -12,10 +14,47 @@ document.getElementById('signin').onclick =() =>{
 document.getElementById('signup').onclick = () =>{
 	signup.style.display = "block";
 }
+document.getElementById('pass').onchange = () =>{
+	let pass = document.getElementById('pass').value;
+	let n = validation(pass);
+	x = (n >= 75)? true : false;
+}
+document.getElementById('continue').onclick = () =>{
+	if(x){
+		signup.style.display = "none";
+		caution.style.display = "block";
+	}
+}
 window.onclick = e => {
 	if (e.target == signin) {
 		signin.style.display = "none";
 	}else if(e.target == signup){
 		signup.style.display = "none";
+	}else if(e.target == caution){
+		caution.style.display = "none";
+	}
+}
+let validation = (word) =>{
+	if(word.length >= 6 & word.length <= 12){
+		let n = 0;
+		n = word.match(/[A-Z]/g)? n+=25 : n+=0;
+		n = word.match(/[a-z]/g)? n+=25 : n+=0;
+		n = word.match(/[0-9]/g)? n+=25 : n+=0;
+		n = word.match(/[$@#&!]/g)? n+=25 : n+=0;
+		if (n == 25) {
+			document.getElementById('error').innerHTML = "password is poor";
+
+		}else if (n == 50) {
+			document.getElementById('error').innerHTML = "password is fair!";
+		}else if (n == 75) {
+			document.getElementById('error').innerHTML = "password is good!";
+		}else if (n == 100) {
+			document.getElementById('error').innerHTML = "password is strong!"
+		}
+		return n;
+	}else if(word.length < 6){
+		document.getElementById('error').innerHTML = "Minimum password length is 6";
+	}else{
+		document.getElementById('error').innerHTML = "Maximum password length is 12";
 	}
 }
