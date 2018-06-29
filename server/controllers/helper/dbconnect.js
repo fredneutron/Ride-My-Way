805ae9be6cccbb9c9084680cefe6e56ef69d4c8d
Adelabu fred <Adelabu4fred@gmail.com>
2018-06-29 22:07:43 +0100

    Updating database config for connection to heroku

--- server/controllers/helper/dbconnect.js
+++ server/controllers/helper/dbconnect.js
@@ -1,11 +1,11 @@
 import { Client } from 'pg';
 
 const client = new Client({
-  host: 'localhost',
+  host: 'ec2-23-23-220-19.compute-1.amazonaws.com',
   port: 5432,
-  user: 'admin',
-  password: 'Password34',
-  database: 'ride_my_way',
+  user: 'vymumfzhkfuoqv',
+  password: '01aedc4d06f6260a361155fa3f9e4beff0032ecef2a2c8361d856ca821a96ab5',
+  database: 'dajhc458kpo4oh',
 });
 
 export default client;
